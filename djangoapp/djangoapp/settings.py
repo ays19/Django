43aca9c4bf2cd5ee406594428e7dbea66a0cbff2
ays19 <sahsanyasir@gmail.com>
2025-05-15 10:07:42 +0600

    path2

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firstdjangoapp',  # my app name here
 ]
 
 MIDDLEWARE = [
