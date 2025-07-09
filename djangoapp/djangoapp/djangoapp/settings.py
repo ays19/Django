a35677839560b20d65303facc7d012d11f1b21d9
ays19 <sahsanyasir@gmail.com>
2025-07-09 08:52:57 +0600

    new

--- djangoapp/djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/djangoapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firstdjangoapp',  # My first Django app
 ]
 
 MIDDLEWARE = [
