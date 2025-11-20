42c562e2a910de01f16e93dc7ec9ef8c7516c791
ays19 <sahsanyasir@gmail.com>
2025-11-20 21:06:34 +0600

    start Django

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2oqr4)_s$oa0cuablzw$ka(jdd)7s7vjrvu(m1wj-31p1oh1z8'
+SECRET_KEY = 'django-insecure-t$$2#vjz0c_u77_hl8*)md9a+%3@x&7(z7()a6o!a=703^hpnj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstdjangoapp',  # My first Django app
 ]
 
 MIDDLEWARE = [
